02e11fbde262c07cb5dd5e74d5a38ecf7742e554
Jessica Castro <jessica.castros@outlook.com>
2023-06-20 16:51:00 -0300

    test: improve slug test with special character validation

--- src/domain/entities/value-objects/slut.test.ts
+++ src/domain/entities/value-objects/slut.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { Slug } from "./slug";
 
 test("should be able to create a new slug from text", () => {
-  const slug = Slug.createFromText("Example question title")
+  const slug = Slug.createFromText("Example question title with special characters!")
   
-  expect(slug.value).toEqual("example-question-title")
+  expect(slug.value).toEqual("example-question-title-with-special-characters")
 })
