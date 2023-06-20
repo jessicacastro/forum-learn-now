d565c132617bda95b1f06c70eeb8634aee6944ed
Jessica Castro <jessica.castros@outlook.com>
2023-06-20 16:24:42 -0300

    chore: add slug value objects prop

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,16 @@
 import { randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
   title: string
   content: string
   authorId: string
+  slug: Slug
 }
 
 export class Question {
   public id: string
+  public slug: Slug
   public title: string
   public content: string
   public authorId: string
@@ -17,5 +20,6 @@ export class Question {
     this.title = props.title
     this.content = props.content
     this.authorId = props.authorId
+    this.slug = props.slug
   }
 }
--- src/domain/use-cases/answer-question/answer-question.test.ts
+++ src/domain/use-cases/answer-question/answer-question.test.ts
@@ -1,10 +1,15 @@
 import { expect, test } from 'vitest'
+import { AnswersRepository } from '../../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
-test('Create an answer', () => {
-  const answerQuestionUseCase = new AnswerQuestionUseCase()
+const answersRepositoryMock: AnswersRepository = {
+  create: async () => {}
+}
 
-  const answer = answerQuestionUseCase.execute({
+test('Create an answer', async () => {
+  const answerQuestionUseCase = new AnswerQuestionUseCase(answersRepositoryMock)
+
+  const answer = await answerQuestionUseCase.execute({
     questionId: '1',
     instructorId: '1',
     content: 'New response to a question',
